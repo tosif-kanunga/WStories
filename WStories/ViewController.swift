8daec58f746f5d410f1ab0b5ac83e59532eee959
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2023-04-12 14:39:52 +0530

    Initial Commit

---
+++ WStories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WStories
+//
+//  Created by shadab sheikh on 2023-04-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
